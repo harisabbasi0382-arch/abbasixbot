aef33b1745f00310904637f79808954fe0262bb8
harisabbasi0382-arch <harisabbasi0382@gmail.com>
2026-07-10 23:00:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-import random
+
 import time
 from datetime import datetime
-
-pairs = [
-    "EUR/USD",
-    "USD/JPY",
-    "GBP/USD",
-    "AUD/USD",
-    "USD/CAD"
-]
 
 print("=== ABBASIXBOT STARTED ===")
 
 while True:
-    pair = random.choice(pairs)
-    signal = random.choice(["🟢 CALL", "🔴 PUT"])
-    confidence = random.randint(70, 95)
-
-    print("\n--------------------------")
-    print("Pair:", pair)
-    print("Signal:", signal)
-    print("Confidence:", str(confidence) + "%")
+    print("-------------------------")
     print("Time:", datetime.now().strftime("%H:%M:%S"))
-
-    time.sleep(15)
+    print("Bot is running...")
+    time.sleep(5)
