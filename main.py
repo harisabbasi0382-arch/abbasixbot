306259616f21944c7ca6c0494253aa1111b4bb3a
harisabbasi0382-arch <harisabbasi0382@gmail.com>
2026-07-10 23:53:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import yfinance as yf
-import pandas as pd
-from ta.trend import EMAIndicator
-from ta.momentum import RSIIndicator
+print("ABBASIXBOT STARTED")
 
-# Get market data
-data = yf.download("EURUSD=X", period="5d", interval="5m")
+signal = "BUY"
 
-# Indicators
-data["EMA9"] = EMAIndicator(data["Close"], window=9).ema_indicator()
-data["EMA21"] = EMAIndicator(data["Close"], window=21).ema_indicator()
-data["RSI"] = RSIIndicator(data["Close"], window=14).rsi()
+print("Signal:", signal)
 
-last = data.iloc[-1]
-
-if last["EMA9"] > last["EMA21"] and last["RSI"] < 70:
-    print("🟢 BUY SIGNAL")
-elif last["EMA9"] < last["EMA21"] and last["RSI"] > 30:
-    print("🔴 SELL SIGNAL")
-else:
-    print("⚪ WAIT")
+print("ABBASIXBOT FINISHED"))
